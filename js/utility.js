b6b688ea0680253102d2ea550bfb4ebc1c864afb
smtariqulislam <s.m.tariqul.islam.eu181400082@gmail.com>
2022-08-24 20:00:18 +0600

    add one common function

---
+++ js/utility.js
@@ -0,0 +1,8 @@
+function getInputId(inputId) {
+
+    const inputField = document.getElementById(inputId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
